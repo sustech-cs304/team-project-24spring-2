31271af703c04e695f4d705f9412e4c0946e97a7
LUN Tianle <786071433@qq.com>
2024-04-12 04:50:47 +0800

    Update alipay payment service.

--- back-end-main/campus-event-system/src/main/java/cn/edu/sustech/ces/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.sustech.ces.repository;
-
-import cn.edu.sustech.ces.entity.Order;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface OrderRepository extends ListCrudRepository<Order, Integer> {
-}
