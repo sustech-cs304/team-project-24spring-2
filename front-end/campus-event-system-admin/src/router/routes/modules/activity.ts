8539f9f0c59738b47eb93df58bcaf5aa54f87734
Zexin Feng <12110104@mail.sustech.edu.cn>
2024-04-15 04:20:07 +0800

    Frontend: Format

--- front-end/campus-event-system-admin/src/router/routes/modules/activity.ts
+++ front-end/campus-event-system-admin/src/router/routes/modules/activity.ts
@@ -2,37 +2,37 @@ import { DEFAULT_LAYOUT } from '../base';
 import { AppRouteRecordRaw } from '../types';
 
 const DASHBOARD: AppRouteRecordRaw = {
-    path: '/activity',
-    name: 'activity',
-    component: DEFAULT_LAYOUT,
-    meta: {
-        locale: 'menu.activity',
+  path: '/activity',
+  name: 'activity',
+  component: DEFAULT_LAYOUT,
+  meta: {
+    locale: 'menu.activity',
+    requiresAuth: true,
+    icon: 'icon-dashboard',
+    order: 1,
+  },
+  children: [
+    {
+      path: 'manage',
+      name: 'manage',
+      component: () => import('@/views/activity/manage/index.vue'),
+      meta: {
+        locale: 'menu.activity.manage',
         requiresAuth: true,
-        icon: 'icon-dashboard',
-        order: 1,
+        roles: ['admin'],
+      },
     },
-    children: [
-        {
-            path: 'manage',
-            name: 'manage',
-            component: () => import('@/views/activity/manage/index.vue'),
-            meta: {
-                locale: 'menu.activity.manage',
-                requiresAuth: true,
-                roles: ['admin'],
-            },
-        },
-        {
-            path: 'create',
-            name: 'create',
-            component: () => import('@/views/activity/create/index.vue'),
-            meta: {
-                locale: 'menu.activity.create',
-                requiresAuth: true,
-                roles: ['admin'],
-            },
-        },
-    ],
+    {
+      path: 'create',
+      name: 'create',
+      component: () => import('@/views/activity/create/index.vue'),
+      meta: {
+        locale: 'menu.activity.create',
+        requiresAuth: true,
+        roles: ['admin'],
+      },
+    },
+  ],
 };
 
 export default DASHBOARD;
