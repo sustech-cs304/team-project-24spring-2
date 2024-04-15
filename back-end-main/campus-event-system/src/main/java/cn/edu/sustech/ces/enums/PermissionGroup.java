4d149b972a52b96b34e3e68f8d15716db7a264ed
LUN Tianle <786071433@qq.com>
2024-04-15 21:28:07 +0800

    Partly Update

---
+++ back-end-main/campus-event-system/src/main/java/cn/edu/sustech/ces/enums/PermissionGroup.java
@@ -0,0 +1,7 @@
+package cn.edu.sustech.ces.enums;
+
+public enum PermissionGroup {
+    USER,
+    ADMIN,
+    SUPER_ADMIN
+}
---
+++ back-end-main/campus-event-system/src/main/java/cn/edu/sustech/ces/security/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package cn.edu.sustech.ces.security;
+
+import cn.edu.sustech.ces.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserService userService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserService userService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request);
+
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)){
+
+            String userInput = this.jwtTokenProvider.getUserInput(token);
+            UserDetails userDetails = this.userService.loadUserByUsername(userInput);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request){
+
+        String bearerToken = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
+    }
+}
