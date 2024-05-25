67246009aee1a8877e6f61f0bcafb9af194991ec
LUN Tianle <786071433@qq.com>
2024-05-26 04:48:24 +0800

    Update global exception handler

--- back-end-main/campus-event-system/src/main/java/cn/edu/sustech/ces/controller/GlobalExceptionHandler.java
+++ back-end-main/campus-event-system/src/main/java/cn/edu/sustech/ces/controller/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package cn.edu.sustech.ces.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +17,21 @@ public class GlobalExceptionHandler {
 
         if (e instanceof AccessDeniedException) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access Denied");
+        }
+        if (e instanceof BadCredentialsException) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bad Credentials");
+        }
+        if (e instanceof NoResourceFoundException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource Not Found");
+        }
+        if (e instanceof IllegalArgumentException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        if (e instanceof IllegalStateException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        if (e instanceof NullPointerException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Null Pointer Exception");
         }
 
         String body = "Internal Server Error\n" + e.getMessage();
