2b469e91bb9dfeef949080094c3e6e0c67ef0f62
Zexin Feng <12110104@mail.sustech.edu.cn>
2024-05-28 13:45:57 +0800

    bug fix

--- front-end/campus-event-system-admin/docker_run.sh
+++ front-end/campus-event-system-admin/docker_run.sh
@@ -5,6 +5,6 @@ docker run --name ces -p 8888:88 \
 -e VITE_PROXY_IMAGES="http://host.docker.internal:19000" \
 -e VITE_PROXY_API="http://host.docker.internal:8080" \
 -e CES_ADMIN_PORT=88 \
--e CES_SERVER_NAME="CES Admin" \
+-e CES_SERVER_NAME="sustc.mark455.cn" \
 -e VITE_API_BASE_URL="http://host.docker.internal" \
  ces-admin:v1.0
